4d41adb7d07314ecb1eeba366ee9749a9e63b28f
Moises Antonio Benvegnu <moisesbenvegnu@gmail.com>
2022-07-15 23:12:01 -0300

    Update excel_to_csv.py

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -16,40 +16,38 @@ uploaded_file = st.file_uploader("Faça o upload do arquivo em excel clicando ab
 
 if uploaded_file is not None:
     
-    try:
     
-        planilha = pd.read_excel(uploaded_file, sheet_name = None)
-    
-        abas_dados = []
-        for aba in planilha.values():
-            abas_dados.append(aba)
-        abas_titulos = []
-        for titulo in planilha:
-            abas_titulos.append(titulo)
-        
-        st.markdown('**O Arquivo em excel possui as seguintes abas que serão convertidas em arquivos CSV:**')
-        
-        st.info(abas_titulos)
-        
-        st.markdown('**Seguem abaixo os arquivos em CSV para download:**')
-    
-        for i in range(len(abas_dados)):
-                
-            st.download_button(
-                 label='Download ' + abas_titulos[i] + '.csv',
-                 data=abas_dados[i].to_csv(index = False, sep = ';').encode('utf-8'),
-                 file_name = abas_titulos[i] + '.csv',
-                 mime='text/csv',
-             )
-            
-            
-            if st.button('Pré-visualizar o arquivo ' + abas_titulos[i] + '.csv'):
-                try:
-                    st.dataframe(abas_dados[i])
-                except:
-                    st.info('Não foi possível pré-visualizar este arquivo')
+    planilha = pd.read_excel(uploaded_file, sheet_name = None)
 
-    except:
-        st.info('Não foi possível realizar a conversão. Verifique se o arquivo utilizado atende aos requisitos para a conversão.')
+    abas_dados = []
+    for aba in planilha.values():
+        abas_dados.append(aba)
+    abas_titulos = []
+    for titulo in planilha:
+        abas_titulos.append(titulo)
+
+    st.markdown('**O Arquivo em excel possui as seguintes abas que serão convertidas em arquivos CSV:**')
+
+    st.info(abas_titulos)
+
+    st.markdown('**Seguem abaixo os arquivos em CSV para download:**')
+
+    for i in range(len(abas_dados)):
+
+        st.download_button(
+             label='Download ' + abas_titulos[i] + '.csv',
+             data=abas_dados[i].to_csv(index = False, sep = ';').encode('utf-8'),
+             file_name = abas_titulos[i] + '.csv',
+             mime='text/csv',
+         )
+
+
+        if st.button('Pré-visualizar o arquivo ' + abas_titulos[i] + '.csv'):
+            try:
+                st.dataframe(abas_dados[i])
+            except:
+                st.info('Não foi possível pré-visualizar este arquivo')
+
+    #st.info('Não foi possível realizar a conversão. Verifique se o arquivo utilizado atende aos requisitos para a conversão.')
 
 st.markdown('Desenvolvido por Moises A. Benvegnu')
